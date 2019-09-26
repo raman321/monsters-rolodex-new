8ff7a8caab792c9b45abfd89f49c195e853b79da
Raman Kumar <raman.kumar14@wipro.com>
2019-09-26 16:38:54 +0530

    React code

---
+++ precache-manifest.838cdeb8661c0f8f0c5f3423a7e0f667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31b76fdf00a88ac7a8d22c7f5ac2400",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "413137cf2cc16e82671d",
+    "url": "/monsters-rolodex/static/css/main.30a24e46.chunk.css"
+  },
+  {
+    "revision": "7b06fdf49ca34b60dc1c",
+    "url": "/monsters-rolodex/static/js/2.7770e699.chunk.js"
+  },
+  {
+    "revision": "413137cf2cc16e82671d",
+    "url": "/monsters-rolodex/static/js/main.9051fa48.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
